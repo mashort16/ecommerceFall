da7eef9da43942f06fc1b5fcae4b0097a703a111
Megan Short <meganalysse.short@gmail.com>
2016-10-19 11:15:40 -0400

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_filter :categories, :brands, :line_items
+
+  def categories
+    @categories = Category.all
+  end
+
+  def brands
+    @brands = Product.pluck(:brand).sort.uniq!
+  end
+
+  def line_items
+    @line_items = LineItem.all
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  get 'view_order' => 'cart#view_order'
+
+  get 'checkout' => 'cart#checkout'
+
+  resources :line_items
+  resources :orders
+  devise_for :users
+  get 'all' => 'storefront#all_items'
+
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
+  resources :categories
+  resources :products
+
+  root 'storefront#all_items'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
